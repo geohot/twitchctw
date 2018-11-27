c334c27ce2a3789efa336c9ef2b175cd4dceaaba
George Hotz <geohot@gmail.com>
2018-11-27 23:07:11 +0800

    actually doing the weighting

--- ctw.py
+++ ctw.py
@@ -17,7 +17,7 @@ def bitgen(x):
 
 from collections import defaultdict
 
-NUMBER_OF_BITS = 24
+NUMBER_OF_BITS = 16
 
 nodes = []
 class Node():
@@ -30,22 +30,46 @@ class Node():
   def __str__(self):
     return "[%d,%d]" % (self.c[0], self.c[1])
     
-  def getp(self, x):
-    np = (self.c[x[-1]] + 0.5) / (self.c[0] + self.c[1] + 1)
-    if self.n[x[-1]] is not None:
-      return self.n[x[-1]].getp(x[:-1])
-      #return 0.5 * np + 0.5 * self.n[x[-1]].getp(x[:-1])
-    return np
+  def getp_recurse(self, x, p):
+    np = (self.c[p] + 0.5) / (self.c[0] + self.c[1] + 1)
+    if x != [] and self.n[x[-1]] is not None:
+      ret = self.n[x[-1]].getp_recurse(x[:-1], p)
+      return ret + [np]
+    return [np]
 
-  def add(self, x):
-    t = x[-1]
-    self.c[t] += 1
-    if x[:-1] == []:
-      # if there's no more string, don't make more nodes
+  def getp(self, x, p):
+    l = self.getp_recurse(x, p)
+    ret = l[-1]
+    for x in l[:-1][::-1]:
+      ret = 0.5 * ret + 0.5*x
+    return ret
+
+  def add(self, x, p):
+    self.c[p] += 1
+    if x == []:
       return
+    t = x[-1]
     if self.n[t] is None:
       self.n[t] = Node()
-    self.n[t].add(x[:-1])
+    self.n[t].add(x[:-1], p)
+
+def quantize(x):
+  x = int(x*256.0 + 0.5)
+  if x == 0:  
+    x = 1
+  if x == 255:
+    x = 254
+  return x
+
+class Encoder():
+  def __init__(self, f):
+    self.l = 0
+    self.h = 256
+
+  def code(self, p, x):
+    print(p, x)
+
+enc = Encoder(open("enwik4.out", "wb"))
 
 root = Node()
 bg = bitgen(enw)
@@ -57,16 +81,17 @@ try:
     cnt += 1
     x = next(bg)
 
-    p_x = root.getp(prevx)
+    # finite precision bro
+    p_0 = root.getp(prevx, 0)
+    p_x = p_0 if x == 0 else (1.0 - p_0)
     H += -math.log2(p_x)
 
     # increment tables
+    root.add(prevx, x)
     prevx.append(x)
     prevx = prevx[-NUMBER_OF_BITS-1:]
-    root.add(prevx)
     if cnt % 5000 == 0:
       print("ratio %.2f%%, %d nodes" % (H*100.0/cnt, len(nodes)))
-
 except StopIteration:
   pass
 
@@ -74,7 +99,7 @@ print("%.2f bytes of entropy, %d nodes" % (H/8.0, len(nodes)))
 #for n in nodes:
 #  print(n)
 
-#exit(0)
+exit(0)
 
 lookup = defaultdict(lambda: [0,0])
 bg = bitgen(enw)
@@ -93,6 +118,10 @@ try:
     # https://en.wikipedia.org/wiki/Krichevsky–Trofimov_estimator
     p_x = (lookup[px][x] + 0.5) / (lookup[px][0] + lookup[px][1] + 1)
     H += -math.log2(p_x)
+
+    # encode
+    #p_0 = (lookup[px][0] + 0.5) / (lookup[px][0] + lookup[px][1] + 1)
+    #enc.code(quantize(p_0), x)
 
     # increment tables
     lookup[px][x] += 1
