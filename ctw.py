d24d7e02013ab3961c84fa0d4c6c0298b7be9d2b
George Hotz <geohot@gmail.com>
2018-11-27 20:40:30 +0800

    putting the tree in CTW: regression

--- ctw.py
+++ ctw.py
@@ -15,14 +15,75 @@ def bitgen(x):
     for i in range(8):
       yield int((c & (0x80>>i)) != 0)
 
+def setgen(x, l):
+  bg = bitgen(x)
+  ret = []
+  while 1:
+    ret.append(next(bg))
+    ret = ret[-l:]
+    if len(ret) == l:
+      yield ret
+
+
 from collections import defaultdict
 
-NUMBER_OF_BITS = 16
+NUMBER_OF_BITS = 4
 
 # https://en.wikipedia.org/wiki/Krichevsky–Trofimov_estimator
 lookup = defaultdict(lambda: [0.5,1.0])
 
-bg = bitgen(enw)
+sg = setgen(enw, NUMBER_OF_BITS)
+
+#bg = bitgen(enw)
+
+# 10101
+# 010101
+# 110101
+
+ncount = 0
+
+class Node():
+  def __init__(self):
+    global ncount
+    self.c = [0,0]
+    self.n = [None, None]
+    ncount += 1
+
+  def getp1(self, x):
+    if self.n[0] is not None and self.n[1] is not None:
+      return self.n[x[-1]].getp1(x[:-1])
+    return (self.c[1] + 0.5) / (self.c[0] + self.c[1] + 1)
+
+  def add(self, x):
+    t = x[-1]
+    self.c[t] += 1
+    if x[:-1] == []:
+      return
+    if self.n[t] is None:
+      self.n[t] = Node()
+    self.n[t].add(x[:-1])
+
+root = Node()
+
+H = 0.0
+cnt = 0
+try:
+  while 1:
+    cnt += 1
+    x = next(sg)
+    p_1 = root.getp1(x)
+    p_x = p_1 if x[-1] == 1 else 1.0 - p_1
+    H += -math.log2(p_x)
+    if cnt%3000 == 0:
+      print("%.2f%% packed, %d nodes" % ((H*100.0)/cnt, ncount))
+    root.add(x)
+except StopIteration:
+  pass
+
+print("%.2f bytes of entropy" % (H/8.0))
+
+exit(0)
+
 HH = 0.0
 try:
   prevx = [-1]*NUMBER_OF_BITS
@@ -38,8 +99,7 @@ try:
 
     #H = -(p_0*math.log2(p_0) + p_1*math.log2(p_1))
 
-    H = -math.log2(p_x)
-    HH += H
+    H += -math.log2(p_x)
 
     # increment tables
     lookup[px][0] += x == 1
@@ -49,6 +109,5 @@ try:
 
 except StopIteration:
   pass
-print("%.2f bytes of entropy" % (HH/8.0))
 
 
