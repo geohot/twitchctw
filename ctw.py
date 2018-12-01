d676a4ec0016b0c4f63a720998ece3f19f1a5a9a
George Hotz <geohot@gmail.com>
2018-12-01 21:45:05 +0800

    still works after refactor

--- ctw.py
+++ ctw.py
@@ -1,142 +1,55 @@
 #!/usr/bin/env python3 
 import sys
 import math
-import numpy as np
-
-def logaddexp(a,b):
-  return np.logaddexp(a,b)
-  #return math.log(math.exp(a) + math.exp(b))
-
-# P(x_i == 1 | x_0:i-1)
-
-# enwik6: 1000000
-#   gzip: 356711
-
-# enwik4: 10000 -> 3728
-
-def clip(x, mn, mx):
-  if x < mn:
-    x = mn
-  if x > mx:
-    x = mx
-  return x
 
 def bitgen(x):
   for c in x:
     for i in range(8):
       yield int((c & (0x80>>i)) != 0)
 
-from collections import defaultdict
+def bytegen(x):
+  for c in x:
+    yield c
 
-NUMBER_OF_BITS = 16
-
-nodes = []
-class Node():
-  def __init__(self, parent=None, symbols=2):
-    global nodes
-    self.c = [0]*symbols
-    self.n = [None]*symbols
-    self.pe = self.pw = 0.0
-    self.parent = parent
-    if self.parent is not None:
-      self.depth = self.parent.depth + 1
-    else:
-      self.depth = 0
-    nodes.append(self)
-
-  def __str__(self):
-    return "[%d,%d]" % (self.c[0], self.c[1])
-
-  def find(self, prevx, create=False):
-    if prevx == []:
-      return self
-    if self.n[prevx[-1]] is None:
-      if create:
-        self.n[prevx[-1]] = Node(self)
-      else:
-        return self
-    return self.n[prevx[-1]].find(prevx[:-1], create)
-
-  def update(self, x, reverse=False):
-    if reverse == False:
-      self.pe += math.log(self.c[x]+0.5) - math.log(sum(self.c)+1.0)
-      self.c[x] += 1
-    else:
-      self.c[x] -= 1
-      self.pe -= math.log(self.c[x]+0.5) - math.log(sum(self.c)+1.0)
-
-    # propagate
-    tpw = 0
-    for nn in self.n:
-      if nn is not None:
-        tpw += nn.pw
-    if tpw == 0:
-      self.pw = self.pe
-    else:
-      self.pw = math.log(0.5) + logaddexp(self.pe, tpw)
-    if self.parent is not None:
-      self.parent.update(x, reverse)
-    
+from model import CTW
 from coder import Coder
 
 def run(fn="enwik4", compress=True):
-  global nodes
-
   if compress:
     enw = open(fn, "rb").read()
     bg = bitgen(enw)
+    #bg = bytegen(enw)
     enc = Coder()
   else:
     enc = Coder(open(fn+".out", "rb").read())
 
-  nodes = []
-  root = Node()
-
+  ctw = CTW()
   H = 0.0
   cnt = 0 
   stream = []
   try:
-    prevx = [0]*NUMBER_OF_BITS
     while 1:
       cnt += 1
 
-      #print(root.pw)
-      pn = root.find(prevx, True)
-
       # what if a wild 0 appeared? this is wrong because creation might happen...
-      prev = pn.pw
-
-      pn.update(0)
-      after_0 = pn.pw
-      pn.update(0, True)
-      p_0 = math.exp(after_0 - prev)
-
-      """
-      pn.update(1)
-      after_1 = pn.pw
-      pn.update(1, True)
-      p_1 = math.exp(after_1 - prev)
-      """
+      p_0 = ctw.log_prob(0)
 
       if compress:
         x = next(bg)
         enc.code(p_0, x)
       else:
         x = enc.code(p_0)
+
       stream.append(x)
 
       p_x = p_0 if x == 0 else (1.0 - p_0)
       H += -math.log2(p_x)
 
-      # increment tables
-      tn = root.find(prevx, create=True)
-      tn.update(x)
+      ctw.update(x)
 
-      prevx.append(x)
-      prevx = prevx[-NUMBER_OF_BITS:]
       if cnt % 5000 == 0:
-        ctw_bytes = (root.pw/math.log(2))/-8
-        print("%5d: ratio %.2f%%, %d nodes, %.2f bytes, %.2f ctw" % (cnt//8, H*100.0/cnt, len(nodes), H/8.0, ctw_bytes))
+        ctw_bytes = (ctw.root.pw/math.log(2))/-8
+        print("%5d: ratio %.2f%%, %d nodes, %.2f bytes, %.2f ctw" % (cnt//8, H*100.0/cnt, len(ctw.nodes), H/8.0, ctw_bytes))
 
       # TODO: make this generic
       if not compress and cnt == 80000:
@@ -144,7 +57,7 @@ def run(fn="enwik4", compress=True):
   except StopIteration:
     pass
 
-  print("%.2f bytes of entropy, %d nodes, %d bits, %d bytes" % (H/8.0, len(nodes), len(stream), len(enc.ob)))
+  print("%.2f bytes of entropy, %d nodes, %d bits, %d bytes" % (H/8.0, len(ctw.nodes), len(stream), len(enc.ob)))
 
   if compress:
     with open(fn+".out", "wb") as f:
---
+++ model.py
@@ -0,0 +1,86 @@
+import math
+import numpy as np
+
+def logaddexp(a,b):
+  return np.logaddexp(a,b)
+
+nodes = []
+class Node():
+  def __init__(self, parent=None, symbols=2):
+    global nodes
+    self.c = [0]*symbols
+    self.n = [None]*symbols
+    self.symbols = symbols
+    self.pe = self.pw = 0.0
+    self.parent = parent
+    if self.parent is not None:
+      self.depth = self.parent.depth + 1
+    else:
+      self.depth = 0
+    nodes.append(self)
+
+  def __str__(self):
+    return "[%d,%d]" % (self.c[0], self.c[1])
+
+  def find(self, prevx, create=False):
+    if prevx == []:
+      return self
+    if self.n[prevx[-1]] is None:
+      if create:
+        self.n[prevx[-1]] = Node(self, self.symbols)
+      else:
+        return self
+    return self.n[prevx[-1]].find(prevx[:-1], create)
+
+  def update(self, x, reverse=False):
+    peadj = math.log(self.c[x]+0.5) - math.log(sum(self.c)+1.0)
+    if reverse == False:
+      self.pe += peadj
+      self.c[x] += 1
+    else:
+      self.c[x] -= 1
+      self.pe -= peadj
+
+    # propagate
+    tpw = 0
+    for nn in self.n:
+      if nn is not None:
+        tpw += nn.pw
+    if tpw == 0:
+      self.pw = self.pe
+    else:
+      self.pw = math.log(0.5) + logaddexp(self.pe, tpw)
+    if self.parent is not None:
+      self.parent.update(x, reverse)
+
+class CTW(object):
+  def __init__(self, context_length=8):
+    global nodes
+    nodes = []
+    self.context_length = context_length
+    self.root = Node(symbols=2)
+    self.prevx = [0]*self.context_length
+
+  @property
+  def nodes(self):
+    return nodes
+
+  def log_prob(self, s):
+    pn = self.root.find(self.prevx, True)
+    prev = pn.pw
+    pn.update(s)
+    after_s = pn.pw
+    pn.update(s, True)
+    p_s = math.exp(after_s - prev)
+    return p_s
+
+  def update(self, x):
+    # increment tables
+    tn = self.root.find(self.prevx, create=True)
+    tn.update(x)
+
+    self.prevx.append(x)
+    self.prevx = self.prevx[-self.context_length:]
+
+
+
